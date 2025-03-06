cc02676a5ffc7ef99a5461fa2895deaf1602c5a9
Abdalla204 <a.mohamed.dev90@gmail.com>
2025-03-07 01:01:27 +0200

    Add files via upload

---
+++ Task_1.py
@@ -0,0 +1,16 @@
+"""
+التكليف رقم واحد
+
+
+"""
+name = "Abdalla"
+age ="19"
+country = "Egypt"
+
+print("Nama:" + name +"\n" +"Age:" + age +"\n" + "Country:" + country)
+
+print("Hello My Name is" +" " +name + " Iam " + age +" " +"Years Old And I Live in"+" " + country )
+
+print(type(name))
+print(type(age))
+print(type(country))
