21f3596b16e2fa1d8080a3d061c528e0baec0b94
Abdalla204 <a.mohamed.dev90@gmail.com>
2025-03-11 04:19:43 +0200

    Add files via upload

---
+++ Task_ 3.py
@@ -0,0 +1,42 @@
+# 
+# التكليف الاول
+# 
+
+print(100) #int
+print(100.0) #Float
+print(100+0j) #complix
+
+# 
+#  اتكليف الثاني
+# 
+
+Mycomplexnamber = 1+2j
+
+print("imaginary part is: {}" .format(Mycomplexnamber.imag))
+print("Rael part is: {}" .format(Mycomplexnamber.real))
+
+#
+#  التكليف الثالث
+# 
+
+num = 10
+print(float(num))
+
+# 
+#  التكليف الرابع
+# 
+
+num = 159.650
+print(int(num))
+num = int(num)
+print(type(num))
+
+# 
+#  التكليف الخامس
+# 
+
+print(100 - 115 == -15)
+print(50 * 30 == 1500)
+print(21 % 4 == 1)
+print(110 / 10 == 11)
+print(97 // 20 == 4)
